19e1bc0471b60a856c44bee2a722282f13ba6796
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2022-12-07 22:11:46 +0000

    Configure and export database client

--- backend/supabase.js
+++ backend/supabase.js
@@ -1,1 +1,9 @@
 require('dotenv').config();
+const { createClient} = require('@supabase/supabase-js');
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseSecret = process.env.SUPABASE_SECRET;
+
+const db = createClient(supabaseUrl, supabaseSecret);
+
+ module.exports = db;
