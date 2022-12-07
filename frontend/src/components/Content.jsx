3f33c533c244dcb7a5f1d9e60001a350c97f80ab
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2022-12-07 14:46:22 +0000

    Add Content component jsx

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -1,7 +1,16 @@
 const Content = () => {
   return (
     <div className="content">
-        Content
+      <h2>New Note</h2>
+      <form>
+        <div className="body">
+          <input type="text" placeholder="Title"/>
+          <textarea rows="15" placeholder="Enter notes..."></textarea>
+        </div>
+        <div className="footer">
+          <button type="submit">Add notes</button>
+        </div>
+      </form>
     </div>
   );
 }
