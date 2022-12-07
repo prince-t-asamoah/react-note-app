4133e3535e17a52a663c68c443ffb90ad5ac1bfd
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2022-12-07 14:27:04 +0000

    Add Notes components

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,8 +1,15 @@
+import Note from "./Note";
+
 const SideBar = () => {
   return (
     <div className="sidebar">
        <div className="search">
         <input type="search" placeholder="Search by Title" />
+      </div>
+      <div className="notes">
+        <Note/>
+        <Note/>
+        <Note/>
       </div>
     </div>
   );
