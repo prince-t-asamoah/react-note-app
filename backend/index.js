5a863ac8d9f557b7ac265990b8451589a40a9211
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2022-12-08 10:33:12 +0000

    Add get route for all notes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { getAllNotes } = require('./controllers');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +12,8 @@ const PORT = process.env.PORT || 4000;
 app.get("/", (_req, res) => {
   res.send('<h1 style="text-align:center;">React Note App</h1>');
 });
+
+app.get('/note/all', getAllNotes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
