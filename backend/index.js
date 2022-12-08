89b0f058b00bda6228d5f006d9b84c9c5a0434be
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2022-12-08 08:07:18 +0000

    Enable cors at app level

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
-const PORT =  process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-    res.send('<h1 style="text-align:center;">React Note App</h1>');
+app.get("/", (_req, res) => {
+  res.send('<h1 style="text-align:center;">React Note App</h1>');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
