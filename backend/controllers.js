fe867c2af82bff18f8c54a2654a05c801c648572
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2022-12-08 10:32:31 +0000

    Create and export function to fetch all notes

--- backend/controllers.js
+++ backend/controllers.js
@@ -1,1 +1,15 @@
 const db = require('./supabase');
+const note = db.from('note');
+
+const getAllNotes = async (_req, res) => {
+  const { data, error } = await note.select('*');
+  if (error) {
+    res.json({error});
+  } else  {
+    res.json(data);
+  }
+};
+
+module.exports = {
+  getAllNotes
+}
