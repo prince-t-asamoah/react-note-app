13dc598cacff4eabc5c7a4f7a38aa3b06d91a1fe
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2022-12-10 15:31:30 +0000

    Add noteSlice to store reducer

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import noteSlice from "./noteSlice";
 
 export const store = configureStore({
-    reducer: {}
+  reducer: {
+    note: noteSlice,
+  },
 });
