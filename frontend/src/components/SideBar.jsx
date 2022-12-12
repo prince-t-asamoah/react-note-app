95e41b55377e0966170554edc9ae0ee214c408ca
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2022-12-12 11:09:20 +0000

    Render all notes when available

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,14 +1,20 @@
 import Note from "./Note";
-import { fetchAllNoteAPI } from "../redux/noteSlice";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { fetchAllNoteAPI, allNotesState, noteStatusState} from "../redux/noteSlice";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const SideBar = () => {
   const dispatch = useDispatch();
+  const allNotes = useSelector(allNotesState);
+  const noteStatus = useSelector(noteStatusState);
+  const [showMessage, setShowMessage] = useState(true);
 
   useEffect(() => {
     dispatch(fetchAllNoteAPI());
-  }, []);
+    if (noteStatus.hasLoaded) {
+      setShowMessage(false);
+    }
+  });
 
   return (
     <div className="sidebar">
@@ -16,9 +22,11 @@ const SideBar = () => {
         <input type="search" placeholder="Search by Title" />
       </div>
       <div className="notes">
-        <Note title={"Item 1"}/>
-        <Note title={"Item 2"}/>
-        <Note title={"Item 3"}/>
+       {
+          showMessage ?
+          <p>{noteStatus.message}</p> : 
+          allNotes.map(note => <Note key={note.id} title={note.title}/>)
+        }
       </div>
     </div>
   );
