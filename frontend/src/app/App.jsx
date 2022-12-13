ea278462183c970227fbd0ecec2976707e4ba270
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2022-12-13 19:36:47 +0000

    Change app div to react fragment

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -3,13 +3,13 @@ import Content from '../components/Content';
 
 function App() {
   return (
-    <div>
+    <>
       <h1>React Note App</h1>
       <div className='container'>
         <SideBar/>
         <Content/>
       </div>
-    </div>
+    </>
   )
 }
 
