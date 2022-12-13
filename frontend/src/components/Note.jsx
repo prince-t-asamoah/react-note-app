8d55fe8ae876839737e30f6660d11f0d762e8b3c
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2022-12-13 13:55:03 +0000

    Import and attach view note reducer to view button

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -1,11 +1,22 @@
-const Note = ({title}) => {
+import { useDispatch } from "react-redux";
+import { view } from "../redux/noteSlice";
+
+const Note = ({id, title}) => {
+  const dispatch = useDispatch();
+
+  const handleView = () => {
+    dispatch(view(id));
+  }
+
   return (
     <div className="note">
      <div className="header">
         <h2>{title}</h2>
       </div>
       <div className="footer">
-        <span className="view button">View</span>
+        <span className="view button" onClick={handleView}>
+          View
+        </span>
         <span className="edit button">Edit</span>
         <span className="delete button">Delete</span>
       </div>
