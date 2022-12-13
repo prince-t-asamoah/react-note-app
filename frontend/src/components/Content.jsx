5de492f06f6ac597d3826ad744a0cba2cbc1c2b8
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2022-12-13 13:49:38 +0000

    Declare state variable for title and content

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
+
 const Content = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+ 
   return (
     <div className="content">
       <h2>New Note</h2>
       <form>
         <div className="body">
-          <input type="text" placeholder="Title"/>
-          <textarea rows="15" placeholder="Enter notes..."></textarea>
+          <input type="text" placeholder="Title" defaultValue={title}/>
+          <textarea rows="15" placeholder="Enter notes..." defaultValue={content}></textarea>
         </div>
         <div className="footer">
           <button type="submit">Add notes</button>
