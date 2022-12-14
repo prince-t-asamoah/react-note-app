87f30b92dd3ee3982c9ec58a132c22463643c133
prince-t-asamoah <79842448+prince-t-asamoah@users.noreply.github.com>
2022-12-14 19:46:40 +0000

    Create and export async function for new note post route

--- backend/controllers.js
+++ backend/controllers.js
@@ -1,15 +1,32 @@
 const db = require('./supabase');
 const note = db.from('note');
+const uuid = require('uuid');
 
 const getAllNotes = async (_req, res) => {
   const { data, error } = await note.select('*');
   if (error) {
-    res.json({error});
+    res.status(500);
   } else  {
     res.json(data);
   }
 };
 
+const createNote = async (req, res) => {
+  const  {title, content} = req.body;
+  const newNote = {
+    id: uuid.v4(),
+    title,
+    content
+  }
+  const { data, error } = await note.insert(newNote).select();
+  if (error) {
+    res.status(500);
+  } else {
+    res.json(data);
+  }
+}
+
 module.exports = {
-  getAllNotes
+  getAllNotes,
+  createNote
 }
